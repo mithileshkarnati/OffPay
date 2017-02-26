bcbb73dd74d7cabd231a64c807a327308de63a98
Mithilesh Karnati <Mithilesh Karnati>
2017-02-26 09:03:58 +0530

    Third Commit

--- offPay/app/src/main/java/com/example/srikarteratipally/offpay/SmsReceiver.java
+++ offPay/app/src/main/java/com/example/srikarteratipally/offpay/SmsReceiver.java
@@ -41,7 +41,6 @@ public class SmsReceiver extends BroadcastReceiver {
                 // Fetch the text message
                 str += msgs[i].getMessageBody().toString();
                 // Newline <img draggable="false" class="emoji" alt="🙂" src="https://s.w.org/images/core/emoji/72x72/1f642.png">
-                str += "\n";
             }
 
             boolean parse = parser(from,str);
@@ -67,39 +66,59 @@ public class SmsReceiver extends BroadcastReceiver {
         }
     }
 
-    boolean parser (String from, String given){
-
+    boolean parser (String from, String given) {
+        given.replace("\n", "");
         String words[] = given.split(" ");
-        Log.d("hfh1f",Integer.toString(words.length));
-        if(!((words.length == 3)||(words.length == 5))){
-            Log.d("hfhf3","nasa");
+
+
+        //Log.d("hfh1f",Integer.toString(words.length));
+
+
+        if (!((words.length == 3) || (words.length == 5) || (words.length == 2))) {
+            Log.d("hfhf3", "nasa");
+            sendSMS(from, "lop1");
             return true;
-        }
+        } else {
+            //Log.d("hfhf",words[1]);
+            if (!words[0].equals("#rayudu")) {
+                sendSMS(from, "lop2");
+                return true;
+            }
+            //else return false;
+            database temp = new database();
 
 
-        //Log.d("hfhf",words[1]);
-        if(!words[0].equals("#rayudu")) return true;
-        //else return false;
+            if(words.length==2) {
+                if (!words[1].equals("balance")) {
+                    sendSMS(from, "lop3");
+                    return true;
+                }
+                temp.balance(from);
+                return false;
+            }
 
-        database temp = new database();
+            else if (words[1].equals("register")) {
+                Log.d("hff", "brr");
+                if (words.length != 3) {
+                    sendSMS(from, "lop3");
+                    return true;
+                }
+                temp.register(from, words[2]);
 
-        if(words[1].equals("register")){
-            Log.d("hff","brr");
-            if(words.length != 3) return true;
-            temp.register(from,words[2]);
-
-            return false;
-        }
-
-        else if(words[1].equals("pay")){
-            if(words.length!=5)return true;
-            temp.pay(from,words[2],words[3],words[4]);
+                return false;
+            } else if (words[1].equals("pay")) {
+                if (words.length != 5) {
+                    sendSMS(from, "lop4");
+                    return true;
+                }
+                temp.pay(from, words[2], words[3], words[4]);
 //            sendSMS(from,"You paid successfully to " + words[2]);
 //            sendSMS(words[2], "You received amount of " + words[3] + " from " + from);
-            return false;
+                return false;
+            }
+            sendSMS(from, "lop5");
+            return true;
         }
-
-        return true;
     }
 
 }
